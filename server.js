601058c882eb9d7b130367dce5c50bbae6dfc480
Carlo De Gregorio <cdegregorio@gmail.com>
2014-06-16 16:20:29 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+// var http = require('http');
+
+// http.get('http://www.google.com', function(response) {
+//   response.pipe(process.stdout);
+// });
+
+
+// // chat server
+// var net = require('net');
+// var connections = [];
+// net.createServer(function(connection) {
+//   connections.push(connection);
+
+//   connection.on('data', function(data) {
+//     process.stdout.write(data);
+//     connections.forEach(function(connection) {
+//       connection.write(data);
+//     })
+//   });
+// }).listen(3030);
+
+
+var http = require('http');
+var path = require('path');
+var fs = require('fs');
+http.createServer(function(req, res) {
+  // var filePath = path.join(__dirname, 'big.html');
+  var filePath = __filename;
+  console.log('%s %s', req.connection.remoteAddress, req.url);
+  var fileStream = fs.createReadStream(filePath);
+  fileStream.pipe(res);
+}).listen(3030);
